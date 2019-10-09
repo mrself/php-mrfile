cb4dc16ba71ed63fcccff160ee1dad9604b0689b
Lightman <yaruk.my@gmail.com>
2019-10-09 08:10:38 +0300

    allow to use different dropbox clients for dropbox file

--- src/DropboxFile.php
+++ src/DropboxFile.php
@@ -3,13 +3,14 @@
 namespace Mrself\File;
 
 use Kunnu\Dropbox\Dropbox;
+use Kunnu\Dropbox\DropboxApp;
 use Mrself\File\Options\DropboxFileOptions;
 use Mrself\Options\Annotation\Option;
 
 class DropboxFile extends AbstractFile implements FileInterface
 {
     /**
-     * @Option()
+     * @Option(required=false)
      * @var Dropbox
      */
     protected $dropboxClient;
@@ -21,6 +22,16 @@ class DropboxFile extends AbstractFile implements FileInterface
      */
     public function save($options, string $localPath)
     {
-        $this->dropboxClient->download($options->getPath(), $localPath);
+        if ($this->dropboxClient) {
+            $client = $this->dropboxClient;
+        } else {
+            $client = new DropboxApp(
+                $options->getClientId(),
+                $options->getSecret(),
+                $options->getToken()
+            );
+        }
+
+        $client->download($options->getPath(), $localPath);
     }
 }
--- src/Options/DropboxFileOptions.php
+++ src/Options/DropboxFileOptions.php
@@ -2,7 +2,49 @@
 
 namespace Mrself\File\Options;
 
+use Mrself\Options\Annotation\Option;
+
 class DropboxFileOptions extends AbstractFileOptions
 {
+    /**
+     * @Option()
+     * @var string
+     */
+    protected $clientId;
 
+    /**
+     * @Option()
+     * @var string
+     */
+    protected $secret;
+
+    /**
+     * @Option()
+     * @var string
+     */
+    protected $token;
+
+    /**
+     * @return string
+     */
+    public function getClientId(): ?string
+    {
+        return $this->clientId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSecret(): ?string
+    {
+        return $this->secret;
+    }
+
+    /**
+     * @return string
+     */
+    public function getToken(): ?string
+    {
+        return $this->token;
+    }
 }
